119d7e4f6925a36dc0b3c8fd0489ad53b923d676
Net Wolf UK <netwolfuk@gmail.com>
2019-08-24 14:46:19 +1200

    Fix up template names, and tidy up display beans.

--- tcwebhooks-web-ui/src/main/java/webhook/teamcity/extension/bean/WebhookConfigAndBuildTypeListHolder.java
+++ tcwebhooks-web-ui/src/main/java/webhook/teamcity/extension/bean/WebhookConfigAndBuildTypeListHolder.java
@@ -7,6 +7,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import webhook.teamcity.BuildStateEnum;
+import webhook.teamcity.payload.PayloadTemplateEngineType;
 import webhook.teamcity.payload.WebHookPayload;
 import webhook.teamcity.payload.WebHookPayloadTemplate;
 import webhook.teamcity.settings.WebHookConfig;
@@ -14,25 +15,26 @@ import webhook.teamcity.settings.WebHookConfig;
 @Getter
 public class WebhookConfigAndBuildTypeListHolder {
 	private String url;
-	private String uniqueKey; 
+	private String uniqueKey;
 	private boolean enabled;
 	private String payloadTemplate;
 	private String payloadFormatForWeb = "Unknown";
 	private List<StateBean> states = new ArrayList<>();
 	private boolean allBuildTypesEnabled;
 	private boolean subProjectsEnabled;
-	
+	private PayloadTemplateEngineType payloadTemplateEngineType;
+
 	@Setter
 	private List<WebhookBuildTypeEnabledStatusBean> builds = new ArrayList<>();
-	
+
 	@Setter
 	private String enabledEventsListForWeb;
 	@Setter
 	private String enabledBuildsListForWeb;
-	
+
 	@Setter
 	private WebhookAuthenticationConfigBean authConfig = null;
-	
+
 	public WebhookConfigAndBuildTypeListHolder(WebHookConfig config, Collection<WebHookPayload> registeredPayloads, List<WebHookPayloadTemplate> templateList) {
 		url = config.getUrl();
 		uniqueKey = config.getUniqueKey();
@@ -43,22 +45,23 @@ public class WebhookConfigAndBuildTypeListHolder {
 		allBuildTypesEnabled = config.isEnabledForAllBuildsInProject();
 		subProjectsEnabled = config.isEnabledForSubProjects();
 		addBuildStatesFromConfig(config);
-		
+
 		if (config.getAuthenticationConfig() != null){
 			this.authConfig = WebhookAuthenticationConfigBean.build(config.getAuthenticationConfig());
 		}
 		WebHookPayloadTemplate t = null;
-		
+
 		for (WebHookPayloadTemplate template : templateList){
 			if (template.getTemplateId().equals(payloadTemplate)){
 				t = template;
 			}
 		}
-			
+
 		if (t != null){
 			for (WebHookPayload payload : registeredPayloads){
 				if (t.supportsPayloadFormat(payload.getFormatShortName())){
 					this.payloadFormatForWeb = t.getTemplateDescription() + " (" + payload.getFormatDescription() + ")";
+					this.payloadTemplateEngineType = payload.getTemplateEngineType();
 					break;
 				}
 			}
@@ -73,7 +76,7 @@ public class WebhookConfigAndBuildTypeListHolder {
 			states.add(new StateBean(state.getShortName(), config.getBuildStates().enabled(state)));
 		}
 	}
-	
+
 	public String getEnabledBuildTypes(){
 		StringBuilder types = new StringBuilder();
 		for (WebhookBuildTypeEnabledStatusBean build : getBuilds()){
@@ -82,11 +85,11 @@ public class WebhookConfigAndBuildTypeListHolder {
 			}
 		}
 		return types.toString();
-		
+
 	}
-	
+
 	public void addWebHookBuildType(WebhookBuildTypeEnabledStatusBean status){
 		this.builds.add(status);
 	}
-	
+
 }
